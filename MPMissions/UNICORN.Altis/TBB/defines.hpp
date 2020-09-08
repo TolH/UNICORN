66adcf8199b00b47e5bbabc51de4c0719a6f187d
Tolh <none@none.com>
2020-09-08 10:38:08 -0400

    FIX SCROLLDOWN TBB GUI COLOR

--- MPMissions/UNICORN.Altis/TBB/defines.hpp
+++ MPMissions/UNICORN.Altis/TBB/defines.hpp
@@ -492,190 +492,42 @@ class RscCombo
 };
 class RscListBox
 {
-	deletable = 0;
-	fade = 0;
-	access = 0;
-	type = 5;
-	rowHeight = 0;
-	colorText[] = 
-	{
-		1,
-		1,
-		1,
-		1
-	};
-	colorDisabled[] = 
-	{
-		1,
-		1,
-		1,
-		0.25
-	};
-	colorScrollbar[] = 
-	{
-		1,
-		0,
-		0,
-		0
-	};
-	colorSelect[] = 
-	{
-		0,
-		0,
-		0,
-		1
-	};
-	colorSelect2[] = 
-	{
-		0,
-		0,
-		0,
-		1
-	};
-	colorSelectBackground[] = 
-	{
-		0.95,
-		0.95,
-		0.95,
-		1
-	};
-	colorSelectBackground2[] = 
-	{
-		1,
-		1,
-		1,
-		0.5
-	};
-	colorBackground[] = 
-	{
-		0,
-		0,
-		0,
-		0.3
-	};
-	soundSelect[] = 
-	{
-		"\A3\ui_f\data\sound\RscListbox\soundSelect",
-		0.09,
-		1
-	};
-	autoScrollSpeed = -1;
-	autoScrollDelay = 5;
-	autoScrollRewind = 0;
-	arrowEmpty = "#(argb,8,8,3)color(1,1,1,1)";
-	arrowFull = "#(argb,8,8,3)color(1,1,1,1)";
-	colorPicture[] = 
-	{
-		1,
-		1,
-		1,
-		1
-	};
-	colorPictureSelected[] = 
-	{
-		1,
-		1,
-		1,
-		1
-	};
-	colorPictureDisabled[] = 
-	{
-		1,
-		1,
-		1,
-		0.25
-	};
-	colorPictureRight[] = 
-	{
-		1,
-		1,
-		1,
-		1
-	};
-	colorPictureRightSelected[] = 
-	{
-		1,
-		1,
-		1,
-		1
-	};
-	colorPictureRightDisabled[] = 
-	{
-		1,
-		1,
-		1,
-		0.25
-	};
-	colorTextRight[] = 
-	{
-		1,
-		1,
-		1,
-		1
-	};
-	colorSelectRight[] = 
-	{
-		0,
-		0,
-		0,
-		1
-	};
-	colorSelect2Right[] = 
-	{
-		0,
-		0,
-		0,
-		1
-	};
-	tooltipColorText[] = 
-	{
-		1,
-		1,
-		1,
-		1
-	};
-	tooltipColorBox[] = 
-	{
-		1,
-		1,
-		1,
-		1
-	};
-	tooltipColorShade[] = 
-	{
-		0,
-		0,
-		0,
-		0.65
-	};
-	class ListScrollBar
-	{
-		color[] = 
-		{
-			1,
-			1,
-			1,
-			1
-		};
-		autoScrollEnabled = 1;
-	};
-	x = 0;
-	y = 0;
-	w = 0.3;
-	h = 0.3;
-	style = 16;
-	font = "RobotoCondensed";
-	sizeEx = "(((((safezoneW / safezoneH) min 1.2) / 1.2) / 25) * 1)";
-	shadow = 0;
-	colorShadow[] = 
-	{
-		0,
-		0,
-		0,
-		0.5
-	};
-	period = 1.2;
-	maxHistoryDelay = 1;
+  access = 0;
+  type = 5;
+  style = 0;
+  w = 0.4;
+  h = 0.4;
+  font = "RobotoCondensed";
+  sizeEx = 0.035;
+  rowHeight = 0;
+  colorText[] = {1,1,1,1};
+  colorDisabled[] = {1,1,1,0.25};
+  colorScrollbar[] = {1,1,1,1};
+  colorSelect[] = {0,0,0,1};
+  colorSelect2[] = {1,0.5,0,1};
+  colorSelectBackground[] = {0.6,0.6,0.6,1};
+  colorSelectBackground2[] = {0.2,0.2,0.2,1};
+  colorBackground[] = {0,0,0,1};
+  maxHistoryDelay = 1.0;
+  soundSelect[] = {"",0.1,1};
+  period = 1;
+  autoScrollSpeed = -1;
+  autoScrollDelay = 5;
+  autoScrollRewind = 0;
+  arrowEmpty = "#(argb,8,8,3)color(1,1,1,1)";
+  arrowFull = "#(argb,8,8,3)color(1,1,1,1)";
+  shadow = 0;
+  class ListScrollBar
+  {
+    color[] = {1,1,1,0.6};
+    colorActive[] = {1,1,1,1};
+    colorDisabled[] = {1,1,1,0.3};
+    thumb = "#(argb,8,8,3)color(1,1,1,1)";
+    arrowEmpty = "#(argb,8,8,3)color(1,1,1,1)";
+    arrowFull = "#(argb,8,8,3)color(1,1,1,1)";
+    border = "#(argb,8,8,3)color(1,1,1,1)";
+    shadow = 0;
+  };
 };
 class RscButton
 {
@@ -1026,20 +878,8 @@ class RscFrame
 	deletable = 0;
 	style = 64;
 	shadow = 2;
-	colorBackground[] = 
-	{
-		0,
-		0,
-		0,
-		0
-	};
-	colorText[] = 
-	{
-		1,
-		1,
-		1,
-		1
-	};
+	colorBackground[] = { 1, 0.1, 0.1, 0.8 };
+	colorText[] = {1,1,1,1};
 	font = "RobotoCondensed";
 	sizeEx = 0.02;
 	text = "";
